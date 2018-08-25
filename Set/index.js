0ef79b17524be6ebd302c10c0efce7d4e91ee7d3
张翔 <zhangxiang@zhangxiangdeMacBook-Pro.local>
2018-08-25 21:11:34 +0800

    set

---
+++ Set/index.js
@@ -0,0 +1,46 @@
+
+
+class Set {
+    constructor(data) {
+        this._set = [];
+        this.size = 0;
+        data && data.forEach((val) => { this.add(val); });
+    }
+
+    add(val) {
+        if (this._set.indexOf(val) === -1) {
+            this._set.push(val);
+            this.size ++;
+        }
+    }
+
+    delete(val) {
+        let index = this._set.indexOf(val);
+        if (index == -1) return false;
+        this._set.splice(index, 1);
+        this.size --;
+        return true;
+    }
+
+    has(val) {
+        return this._set.indexOf(val);
+    }
+
+    clear() {
+        this._set = [];
+        this.size = 0;
+    }
+
+    forEach(callback, thisArg = global) {
+        for (let i = 0; i < this.size; i++) {
+            // value 与 key 相同
+            callback.call(thisArg, this._set[i], this._set[i], this._set);
+        }
+    }
+}
+
+let set = new Set([1,'1', 2, '2', 2]);
+
+console.log(set);
+set.add(3);
+console.log(set);
