791b5e57f2eb34eaa14583788afc51346131b6bf
zhangxiang <958033967@qq.com>
2018-09-15 21:40:13 +0800

    update

--- update/index.js
+++ update/index.js
@@ -1,6 +1,38 @@
-const update = (data, pattern) => {
-    let needUpdate = Object.keys(pattern);
+/**
+ * update
+ * @param {*} state 
+ * @param {*} pattern 
+ * 指令
+ * $set 整体替换
+ * $unset 将数组中所有键去掉 (array of string)
+ * $push 将所有元素放入数组中 (array)
+ * $unshift 将所有元素放入数组中 (array)
+ * $splice 目标调用 splice (array of array)
+ * $merge 合并
+ * $apply 使用函数的返回值来更新
+ * $add (array of objects)
+ * $remove (array of string)
+ * 
+ * extend 用于拓展指令
+ */
+const $set = (state, paths, value) => {
     
+};
+
+const update = (state, pattern) => {
+    let needUpdate = Object.keys(pattern);
+    let allKeys = Object.keys(state);
+    let nextState = {};
+    for (let key of allKeys) {
+        if (!needUpdate.includes(key)) {
+            nextState[key] = state[key];
+        } else {
+            let updateOperation = pattern[key];
+            let $set = updateOperation['$set'];
+            nextState[key] = $set;
+        }
+    }
+    return nextState;
 };
 
 var state = { name: 'Alice', todos: [] };
