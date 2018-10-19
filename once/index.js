7fdaeeb4e7781bfe2b4088099e47a6cb34a50211
zhangxiang <958033967@qq.com>
2018-10-19 23:00:55 +0800

    unit

--- once/index.js
+++ once/index.js
@@ -1,7 +1,9 @@
 const once = function (func) {
     let result;
+    let already = false;
     return function () {
-        if (!result) {
+        if (!already) {
+            already = true;
             result = func();
             return result;
         } else {
@@ -13,10 +15,15 @@ const once = function (func) {
 function bootstrapApp() {
     console.log('this should be shown once');
     return 22;
-  }
-  
+}
 var initialize = once(bootstrapApp);
 console.log(initialize());
 console.log(initialize());
-
+var args;
+    var init = once(function() {
+      args = [].slice.call(arguments);
+    });
+    init(11, 22, 33);
+    init(22, 33, 44);
+console.log(args);
 module.exports = once;
