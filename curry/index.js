27905f19b7668f2c0d51c74198f21df216cf6fc1
zhangxiang <958033967@qq.com>
2018-10-20 13:41:15 +0800

    unit test6

--- curry/index.js
+++ curry/index.js
@@ -1,14 +1,33 @@
+// const curry = (func) => {
+//     let allArgs = [];
+//     return function callback (...args) {
+//         allArgs = allArgs.concat(args);
+//         let applyArgs = [];
+//         if (allArgs.length < func.length) {
+//             return curry(callback);
+//         } else {
+//             applyArgs = allArgs;
+//             allArgs = [];
+//             return func.apply(null, applyArgs);
+//         }
+//     }
+// };
+
 const curry = (func) => {
-    let allArgs = [];
+    let argLength = func.argLength || func.length;
+    func.args = func.args || [];
     return function callback (...args) {
-        allArgs = allArgs.concat(args);
-        if (allArgs.length < func.length) {
-            return callback;
+        let thisArgs = func.args.concat(args);
+        if (thisArgs.length < argLength) {
+            callback.args = thisArgs;
+            callback.argLength = argLength;
+            callback.origin = func.origin || func;
+            return curry(callback);
         } else {
-            return func.apply(null, allArgs);
+            return func.origin ? func.origin.apply(null, thisArgs) : func.apply(null, thisArgs);
         }
     }
-};
+}
 
 function add(a, b) {
     return a + b;
@@ -16,5 +35,11 @@ function add(a, b) {
   
 var curried = curry(add);
 console.log(  curried(1)(2)  ); // 3
-
+var add = curry(function(a, b, c, d) {
+    return a + b + c + d;
+});
+var firstTwo = add(1)(2);
+console.log(firstTwo(3)(4)); // 10
+var firstThree = firstTwo(5);
+console.log(firstThree(6)); // 14
 module.exports = curry;
