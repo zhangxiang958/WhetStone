a7827e267a23bf08958bb36790e79ff120f28f38
zhangxiang <958033967@qq.com>
2018-11-05 19:11:18 +0800

    async

--- async/index.js
+++ async/index.js
@@ -55,35 +55,35 @@ const async = {
 
 var getUser = function(userId) {
     return function(cb) {
-      setTimeout(function() {
-        cb(null, {userId: userId, name: 'Joe'});
-      }, Math.random() * 100);
+        setTimeout(function() {
+            cb(null, {userId: userId, name: 'Joe'});
+        }, Math.random() * 100);
     };
-  };
+};
   
-  var upperCaseName = function(cb, user) {
+var upperCaseName = function(cb, user) {
     cb(null, user.name.toUpperCase());
-  };
+};
   
-  var userThunk = getUser(22);
+var userThunk = getUser(22);
   
-  async.sequence([userThunk, upperCaseName])(function(err, data) {
+async.sequence([userThunk, upperCaseName])(function(err, data) {
     console.log(data); // JOE
-  });
+});
   
-  var userThunk1 = getUser(1);
-  var userThunk2 = getUser(2);
+var userThunk1 = getUser(1);
+var userThunk2 = getUser(2);
   
-  async.parallel([userThunk1, userThunk2])(function(err, users) {
+async.parallel([userThunk1, userThunk2])(function(err, users) {
     console.log(users); // [ { userId: 1, name: 'Joe' }, { userId: 2, name: 'Joe' } ]
-  });
-  
-  var faster = function(cb) {
+});
+
+var faster = function(cb) {
     setTimeout(cb.bind(null, null, "I'm faster"), 10);
-  }
-  async.race([userThunk1, faster])(function(err, winner) {
+}
+async.race([userThunk1, faster])(function(err, winner) {
     console.log(winner); // I'm faster
-  });
+l});
 
 
 module.exports = async;
