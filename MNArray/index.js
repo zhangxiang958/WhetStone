9b78c1c9836db757dd14ebd751c1de71c8d83e67
zhangxiang <958033967@qq.com>
2018-11-24 12:13:03 +0800

    feat(m,n): done

---
+++ MNArray/index.js
@@ -0,0 +1,25 @@
+/**
+ * initArray
+ */
+
+const initArray = function (m, n) {
+    let array = [];
+    array.length = m;
+    let proxy = new Proxy(array, {
+        get: function (target, name) {
+            let idx = parseInt(name, 10);
+            if (!Number.isNaN(idx) && idx < m) {
+                return n;
+            } else {
+                return target[name];
+            }
+        }
+    });
+    return proxy;
+};
+
+const initArray = function (m, n) {
+    return new Array(m).fill(n);
+}
+
+initArray(10, 10);
